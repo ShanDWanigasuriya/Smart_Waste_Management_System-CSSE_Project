384a38476f735d1488c7be3480bce3f84f0596d1
Malshan Rathnayake <malshan.edu@gmail.com>
2024-10-14 11:59:20 +0530

    biometric waste management solution

---
+++ biometric_waste_managment_solution/bwms_core_api/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bwms_core_api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EmployeeController : ControllerBase
+	{
+		[HttpPost("Register")]
+		public async Task<IActionResult> Register(Employee employee)
+		{
+			return Ok();
+		}
+
+		[HttpGet("GetEmployeeByDeviceId")]
+		public async Task<IActionResult> GetEmployeeByDeviceId(string deviceId)
+		{
+			return Ok(new Employee());
+		}
+	}
+}
